907e3ca4b85d7a843a01c73ac8693ca05bb3eafe
Sofia <>
2020-12-05 17:41:51 -0500

    Walk in line

--- Assets/_Jam/Scripts/Character.cs
+++ Assets/_Jam/Scripts/Character.cs
@@ -12,12 +12,34 @@ public class Character : MonoBehaviour
 
     private Animator animator;
     private CharacterIKControl ikControl;
+
+    //Movement
+    [Header("Movement")]
+    private bool canMove = false;
+    private Transform target;
+    [SerializeField] private float speed = 1.5f;
+    [SerializeField] private float stoppingDistance = 0.5f;
+    public Transform spotToHoldHands;
     
     private void Awake()
     {
         animator = GetComponentInChildren<Animator>();
         ikControl = GetComponentInChildren<CharacterIKControl>();
         Inactive();
+    }
+
+    private void Update()
+    {
+        if (canMove && target)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+            if (Vector3.Distance(transform.position, target.position) < stoppingDistance)
+            {
+                StopMoving();
+            }
+        }
     }
 
     private void Inactive()
@@ -31,7 +53,7 @@ public class Character : MonoBehaviour
     {
         isCommunity = true;
         darkness.SetActive(false);
-        GameManager.instance.AddToCommunity(this);
+        StartCoroutine(GameManager.instance.AddToCommunity(this));
     }
 
     public void HoldWithRightHand(Transform left)
@@ -52,6 +74,23 @@ public class Character : MonoBehaviour
     public Transform GetRightHand()
     {
         return rightHand;
+    }
+
+    public void WalkTo(Character other)
+    {
+        //todo walk to
+        canMove = true;
+        target = other.spotToHoldHands;
+        animator.SetFloat("speed", 1.0f);
+        transform.LookAt(target);
+    }
+
+    public void StopMoving()
+    {
+        canMove = false;
+        target = null;
+        animator.SetFloat("speed", 0.0f);
+        transform.localEulerAngles = new Vector3(0, 180f, 0);
     }
     
     public void Select()
--- Assets/_Jam/Scripts/GameManager.cs
+++ Assets/_Jam/Scripts/GameManager.cs
@@ -19,18 +19,33 @@ public class GameManager : MonoBehaviour
 
     }
 
-    public void AddToCommunity(Character characterToAdd)
+    public IEnumerator AddToCommunity(Character characterToAdd)
     {
-        
+        //Place shadow so at the end it reads something
         Instantiate(decalPrefab, characterToAdd.transform.position + (Vector3.up * 1.8f), Quaternion.Euler(90, 0,0 ));
         
+        //Add the character
         int communityCount = charactersInCommmunity.Count;
         if (communityCount >= 1)
         {
+            //Last character walks up to the new one
+            charactersInCommmunity[communityCount-1].WalkTo(characterToAdd);
+            
+            for (int i = communityCount-2; i >= 0 ; i--)
+            {
+                yield return  new WaitForSeconds(0.2f);
+                charactersInCommmunity[i].WalkTo(charactersInCommmunity[i+1]);
+            }
+            
+            //TODO MOVE THIS TO WALK TO IN A COUROUTINE
+            //Make them hold hands with each other
             characterToAdd.HoldWithRightHand(charactersInCommmunity[communityCount-1].GetLeftHand());
             charactersInCommmunity[communityCount-1].HoldWithLeftHand(characterToAdd.GetRightHand());
         }
         charactersInCommmunity.Add(characterToAdd);
+        
+       
+      
     }
     
 }
