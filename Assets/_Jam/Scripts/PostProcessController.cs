b54da2bf7408579ce81a62f1795e77242167bb57
squid <luisquid.dev@gmail.com>
2020-12-06 16:36:02 -0500

    Main Menu

---
+++ Assets/_Jam/Scripts/PostProcessController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class PostProcessController : MonoBehaviour
+{
+	private Volume gameVolume;
+	private DepthOfField depthProperty;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+	    gameVolume = GetComponent<Volume>();
+	    gameVolume.profile.TryGet(out depthProperty);
+    }
+
+	public void TurnOffDepth(bool _active){
+		depthProperty.active = _active;
+	}
+}
